b49385971ff1a0b36ec50e2c508c8128fbf7b6fe
E Jolly <39107604+ejolly5678@users.noreply.github.com>
2024-06-26 20:46:02 -0400

    Remove unneeded story graph fields from dictionary #1

--- main/main.py
+++ main/main.py
@@ -20,3 +20,17 @@ with open(filename, encoding="utf8", newline='') as csvfile:
     for row in bookreader:
         story_graph_list.append(row)
 
+#Delete rows that are used in story graph, but not used in goodreads:
+for row in story_graph_list:
+    story_graph_list[row].pop('Read Count', None)
+    story_graph_list[row].pop('Moods', None)
+    story_graph_list[row].pop('Pace', None)
+    story_graph_list[row].pop('Character- or Plot-Driven?', None)
+    story_graph_list[row].pop('Loveable Characters?', None)
+    story_graph_list[row].pop('Diverse Characters?', None)
+    story_graph_list[row].pop('Flawed Characters?', None)
+    story_graph_list[row].pop('Star Rating', None)
+    story_graph_list[row].pop('Content Warnings', None)
+    story_graph_list[row].pop('Content Warning Description', None)
+    story_graph_list[row].pop('Owned?', None)
+    print(story_graph_list[row])
